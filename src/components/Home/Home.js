2d1ea6cae3045ca8cbce8825ddeab38afd637bf7
ajijul-kazi <ajijulkazi0178@gmail.com>
2022-06-23 03:25:12 +0600

    used react fontawesome

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,65 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import{faCoffee,faChevronRight} from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
+import img from '../../image/result.svg';
+import './Home.css';
 const Home = () => {
     return (
         <div>
-            <div className='img'>
-                <img src="/src/image/home-img.svg" alt="" />
+            <section className='home'  id='home'>
+            
+            <div className='image'>
+                <img src={img}  alt="" />
             </div>
             <div className="content">
-                <h2>stay safe, stay healthy</h2>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus iusto ipsum commodi magnam odit officia!</p>
+                <h3>stay safe, stay healthy</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus iusto ipsum c
+                    ommodi magnam odit officia!</p>
+
+                {/*<a href="#" className='btn'>Contact Us <span className='fas fa-chevron-right'></span> </a>*/}
+                <NavLink className='btn' to='/blog'>Contact Us <FontAwesomeIcon className='btn-icon' icon={faChevronRight} /></NavLink>
                 
-
-
-
+                
             </div>
+
             
-        </div>
+            
+    
+        </section>
+        <section>
+            <div className='icons-container' id='icon'>
+            <div className='icons'>
+            <i className='fas fa-user-md'></i>
+            <h3>140+</h3>
+            <p>Doctor At Work</p>
+            </div>
+
+            <div className='icons'>
+            <i className='fas fa-users'></i>
+            <h3>1040+</h3>
+            <p>Satisfied Patients</p>
+            </div>
+
+            <div className='icons'>
+            <i className='fas fa-procedures'></i>
+            <h3>500+</h3>
+            <p>Bed Facility </p>
+            </div>
+
+            <div className='icons'>
+            <i className='fas fa-hospital'></i>
+            <h3>80+</h3>
+            <p>Available Hospital</p>
+            </div>
+            </div>
+        </section>
+        
+        
+        
+</div>
+        
+        
     );
 };
 
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import{faChevronRight,faTruckMedical, faHandHoldingMedical, faUserDoctor, faPills, faProcedures, faHeartbeat} from '@fortawesome/free-solid-svg-icons';
+import './Services.css';
 
 const Services = () => {
     return (
         <div>
-            <h2>This is all services.</h2>
+            <section className='services' id='services'>
+                <h1 className='heading'>Our <span>Services</span></h1>
+                <div className='box-container'>
+
+                        <div className='box'>
+                            <div className='icon-btn'><FontAwesomeIcon  icon={faHandHoldingMedical} /></div>
+                        
+                            <h3>Free Checkups</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Nam accusantium libero quis expedita atque!</p>
+                                <NavLink className='btn' to='/blog'>Learn More <FontAwesomeIcon className='btn-icon' icon={faChevronRight} /></NavLink>
+                        </div>
+
+                        <div className='box'>
+                        <div className='icon-btn'><FontAwesomeIcon  icon={faTruckMedical} /></div>
+                            <h3>24/7 Ambulance</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Nam accusantium libero quis expedita atque!</p>
+                                <NavLink className='btn' to='/blog'>Learn More <FontAwesomeIcon className='btn-icon' icon={faChevronRight} /></NavLink>
+                        </div>
+
+                        <div className='box'>
+                        <div className='icon-btn'><FontAwesomeIcon  icon={faUserDoctor} /></div>
+                            <h3>Expert Doctors</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Nam accusantium libero quis expedita expedita atque!</p>
+                                <NavLink className='btn' to='/blog'>Learn More <FontAwesomeIcon className='btn-icon' icon={faChevronRight} /></NavLink>
+                        </div>
+
+                        <div className='box'>
+                        <FontAwesomeIcon className='icon-btn' icon={faPills} />
+                            <h3>Medicines</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Nam accusantium libero quis expedita atque!</p>
+                                <NavLink className='btn' to='/blog'>Learn More <FontAwesomeIcon className='btn-icon' icon={faChevronRight} /></NavLink>
+                        </div>
+
+                        <div className='box'>
+                        <FontAwesomeIcon className='icon-btn' icon={faProcedures} />
+                            <h3>Bed Facility</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Nam accusantium libero quis expedita atque!</p>
+                                <NavLink className='btn' to='/blog'>Learn More <FontAwesomeIcon className='btn-icon' icon={faChevronRight} /></NavLink>
+                        </div>
+
+                        <div className='box'>
+                        <FontAwesomeIcon className='icon-btn' icon={faHeartbeat} />
+                            <h3>Total Care</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Nam accusantium libero quis expedita atque!</p>
+                                <NavLink className='btn' to='/blog'>Learn More <FontAwesomeIcon className='btn-icon' icon={faChevronRight} /></NavLink>
+                        </div>
+                </div>
+            </section>
         </div>
     );
 };
